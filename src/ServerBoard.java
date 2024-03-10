8c4357301d570b7b732ada245ab52668d2549528
vicbentu <victor.gonzalez.bentue@estudiantat.upc.edu>
2024-03-10 21:23:34 +0100

    Board base

--- src/ServerBoard.java
+++ src/ServerBoard.java
@@ -1,5 +1,13 @@
+import aima.util.Pair;
+
+import java.util.ArrayList;
+
 public class ServerBoard {
+
+    public ArrayList<ArrayList<Pair>> solution; // file, server
+
     public ServerBoard() {
+        this.solution = new ArrayList<>();
     }
 
     public int[] getBoard() {
