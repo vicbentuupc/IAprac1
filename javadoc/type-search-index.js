e47e09f9e367329016a172dd21d2d3f0999a6ca0
shinto machado furuichi <shinto.machado@estudiant.upc.edu>
2024-03-11 17:21:21 +0100

    board implement

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"IA.DistFS","l":"Requests"},{"p":"IA.DistFS","l":"Servers"},{"p":"IA.DistFS","l":"testDistFS"},{"p":"IA.DistFS","l":"Servers.WrongParametersException"}]
--- src/ServerBoard.java
+++ src/ServerBoard.java
@@ -1,20 +1,56 @@
-import aima.util.Pair;
+import IA.DistFS.Requests;
+import IA.DistFS.Servers;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import java.util.Map;
 
 public class ServerBoard {
 
-    public ArrayList<ArrayList<Pair>> solution; // file, server
+    //representation
+    public ArrayList<Map<int,int>> solution; // Usr -> [file, server] (es un map, para agilizar el changeServ)
 
-    public ServerBoard() {
-        this.solution = new ArrayList<>();
+    //constructor(initial solution)
+    //mode == 1(random), mode == 2(nearest server)
+    public ServerBoard(int mode, Requests r, Servers s, int usr_num) { //pasarle el #usr(es un parametro que tu das en el main)
+        if(mode == 1){
+            int n = r.size();
+            solution.resize(usr_num);
+            for(int i = 0; i<n; i++){
+                int[] aux = r.getRequest(i);
+                int rand_serv = random_server(aux[1], s);
+                solution[aux[0]].(new int[]{aux[1], rand_serv});
+            }
+        }
+        else if(mode == 2){
+
+        }
     }
 
-    public int[] getBoard() {
-        return new int[0];
+    //"operators functions"
+    public void change_serv(int usr, int file, int serv, Server s){
+        solution[usr][file] = serv;
     }
 
-    //Constructor
+    //"aplicability functions"
+    public boolean file_in_serv(int file, int serv, Server s){
+        Set servs = (Set) s.fileLocations(f);
+        if(servs.contains(serv))return true;
+        else return false;
+    }
 
+    //"auxiliar functions"
+
+    //return a random serverID which contains the fileID f
+    public int random_server(int f, Servers  s) {
+        Set servs = (Set) s.fileLocations(f);
+        int size = servs.size();
+        // Generate random integers in range 0 to size
+        Random rand = new Random();
+        int rand_int1 = rand.nextInt(size);
+        return servs[rand_int1];
+    }
 
 }
