f915abf8586a1129e6a49cc8de8e5c1fde0d0d19
vicbentu <victor.gonzalez.bentue@estudiantat.upc.edu>
2024-03-12 15:50:15 +0100

    Solucio inicial implementada

--- src/Main.java
+++ src/Main.java
@@ -15,40 +15,33 @@ public class Main {
     public static void main(String[] args) throws Exception{
 
         int users = 10;
-        int requests = 10;
+        int requests = 3;
         int servers = 10;
         int nrep = 3;
         int seed = new Date().hashCode();
+        Requests r = new Requests(users, requests, seed);
+        Servers s = new Servers(servers, nrep, seed);
 
-        ServerBoard board = new ServerBoard(1, new Requests(users, requests, seed), new Servers(servers, nrep, seed), users);
+        ServerBoard board = new ServerBoard(1, r, s, users);
 
-        // Create the Problem object
         Problem p = new  Problem(board,
                 new ServerSuccessorFunction(),
                 new ServerGoalTest(),
                 new ServerHeuristicFunction());
-        
 
-        // Instantiate the search algorithm
-        // Hillclimbing o simulated annealing (s'haura de canviar main pro ara per provar)
         // Search alg = new SimulatedAnnealingSearch(200000, 10, 5, 0.001);
         Search alg = new HillClimbingSearch();
-        // Instantiate the SearchAgent object
         SearchAgent agent = new SearchAgent(p, alg);
 
-        // We print the results of the search
         System.out.println();
         printActions(agent.getActions());
         printInstrumentation(agent.getInstrumentation());
 
-        // You can access also to the goal state using the
-        // method getGoalState of class Search
 
+        for (int i = 0; i < board.solution.size(); i++) System.out.println("User " + i + " -> " + board.solution.get(i));
+        for (int i = 0; i < r.size(); ++i) System.out.println("File " + r.getRequest(i)[1] + ": " + s.fileLocations(r.getRequest(i)[1]));
+        for (int i = 0; i < ServerBoard.user2file.size(); ++i) System.out.println("User " + i + " wants files: " + ServerBoard.user2file.get(i));
 
-
-        for (int i = 0; i < board.solution.size(); i++) {
-            System.out.println("User " + i + " -> " + board.solution.get(i));
-        }
 
     }
 
--- src/ServerBoard.java
+++ src/ServerBoard.java
@@ -1,39 +1,40 @@
 import IA.DistFS.Requests;
 import IA.DistFS.Servers;
 
-import java.util.Map;
-import java.util.Random;
-import java.util.Set;
-import java.util.Vector;
+import java.util.*;
 
 public class ServerBoard {
 
     //representation
-    public Map<Integer, Integer> user2index; // Usr -> [file, server]
-    public Vector<Vector<Integer>> serverBoard;
+    static public Map<Integer, Integer> user2index; // Usr -> [file, server]
+    static public Vector<Vector<Integer>> user2file; // which file in [user][position]
     public Vector<Vector<Integer>> solution; // Usr -> [file, server] (es un map, para agilizar el changeServ)
 
     //constructor(initial solution)
     //mode == 1(random), mode == 2(nearest server)
     public ServerBoard(int mode, Requests r, Servers s, int usr_num) { //pasarle el #usr(es un parametro que tu das en el main)
-        if(mode == 1){
-            int n = r.size();
-            serverBoard = new Vector<>(usr_num);
-            for (int i = 0; i < usr_num; i++) serverBoard.add(new Vector<Integer>(n));
-            for (int i = 0; i < usr_num; i++) {
-                int[] aux = r.getRequest(i);
-                //serverBoard.get(aux[0]).add(aux[1]);
-                System.out.println("User " + i + " -> " + aux[0] + " " + aux[1]);
+        user2index = new HashMap<Integer, Integer>();
+        user2file = new Vector<>(usr_num);
+        for (int i = 0; i < usr_num; i++) user2file.add(new Vector<Integer>(0));
+        solution = new Vector<>(usr_num);
+        for (int i = 0; i < usr_num; i++) solution.add(new Vector<Integer>(0));
+        Integer indexAux = -1;
+        for (int i = 0; i < r.size(); i++) {
+            //static
+            int aux[] = r.getRequest(i);
+            if (!user2index.containsKey(aux[0])) {
+                indexAux++;
+                user2index.put(aux[0], indexAux);
             }
-            //for (int i = 0; i < usr_num; i++) Collection.sort(serverBoard.get(i));
+            System.out.println(indexAux);
+            System.out.println(aux[1]);
 
-
-            solution = new Vector<>(usr_num);
-            for (int i = 0; i < usr_num; i++) solution.add(new Vector<Integer>());
-            for (int i = 0; i < usr_num; i++) {
-                for (int j = 0; j < serverBoard.get(i).size(); j++) {
-                    int rand_serv = random_server(serverBoard.get(i).get(j), s);
-                    solution.get(i).add(rand_serv);
+            user2file.get(indexAux).add(aux[1]);
+            }
+        if(mode == 1) {
+            for (int i = 0; i < user2file.size(); i++) {
+                for (int j = 0; j < user2file.get(i).size(); j++) {
+                    solution.get(i).add(random_server(user2file.get(i).get(j), s));
                 }
             }
         }
@@ -43,8 +44,8 @@ public class ServerBoard {
     }
 
     //"operators functions"
-    public void change_serv(int usr, int file, int serv, Servers s){
-        //solution[usr][file] = serv;
+    public void change_serv(int usrind, int fileind, int serv, Servers s){
+        solution.get(usrind).set(fileind, serv);
     }
 
     //"aplicability functions"
@@ -60,18 +61,12 @@ public class ServerBoard {
     public int random_server(int f, Servers  s) {
         Set servs = (Set) s.fileLocations(f);
         int size = servs.size();
-        // Generate random integers in range 0 to size
+        //Generate random integers in range 0 to size
         Random rand = new Random();
         int rand_int1 = rand.nextInt(size);
         return (int) servs.toArray(new Integer[size])[rand_int1];
     }
 
-    public int[] getBoard(){ //no funciona
-        int[] board = new int[serverBoard.size()];
-        for (int i = 0; i < serverBoard.size(); i++) {
-            //board[i] = serverBoard.get(i);
-        }
-        return board;
-    }
+
 
 }
