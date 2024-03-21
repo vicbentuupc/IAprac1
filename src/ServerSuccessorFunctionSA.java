6712dff42c4c58dbe33b4c2bc133e1065c45b9c3
vicbentu <victor.gonzalez.bentue@estudiantat.upc.edu>
2024-03-21 20:39:17 +0100

    Both Successor functions

--- src/ServerSuccessorFunctionSA.java
+++ src/ServerSuccessorFunctionSA.java
@@ -3,25 +3,27 @@ import aima.search.framework.SuccessorFunction;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ServerSuccessorFunctionSA implements SuccessorFunction {
 
     public List<Successor> getSuccessors(Object state) {
+        Random rand = new Random();
         List<Successor> list = new ArrayList<Successor>();
         ServerBoard board = (ServerBoard) state;
-        for (int i = 0; i < board.solution.size(); ++i) {
-            for (int j = 0; j < board.solution.get(i).size(); ++j) {
-                Integer[] serversWithFile = ServerBoard.servers.fileLocations(ServerBoard.user2file.get(i).get(j)).toArray(new Integer[0]);
-                for (int k = 0; k < serversWithFile.length; ++k) {
-                    if (serversWithFile[k] == board.solution.get(i).get(j)) continue;
-                    ServerBoard newBoard = new ServerBoard(board);
-                    newBoard.change_serv(i, j, serversWithFile[k]);
-                    list.add(new Successor("User " + i + " changes file in index " + j + " from server " + board.solution.get(i).get(j) + " to server " + serversWithFile[k], newBoard));
-                }
-            }
-        }
-
-        //list.add(new Successor("Successor", newBoard));
+        int i = rand.nextInt(board.solution.size());
+        int j;
+        do {
+            j = rand.nextInt(board.solution.get(i).size());
+        } while (j == i);
+        Integer[] serversWithFile = ServerBoard.servers.fileLocations(ServerBoard.user2file.get(i).get(j)).toArray(new Integer[0]);
+        int k;
+        do {
+            k = rand.nextInt(serversWithFile.length);
+        } while (k == board.solution.get(i).get(j));
+        ServerBoard newBoard = new ServerBoard(board);
+        newBoard.change_serv(i, j, serversWithFile[k]);
+        list.add(new Successor("User " + i + " changes file in index " + j + " from server " + board.solution.get(i).get(j) + " to server " + serversWithFile[k], newBoard));
         return list;
     }
 
